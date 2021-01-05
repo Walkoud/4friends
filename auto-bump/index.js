01f1fe9a7792db81412abe36dd934a0868ce3451
Walkoud <38588921+Walkoud@users.noreply.github.com>
2021-01-05 19:42:35 +0100

    Update index.js

--- auto-bump/index.js
+++ auto-bump/index.js
@@ -1,1 +1,26 @@
 
+const Discord = require("discord.js") 
+const client = new Discord.Client({disableEveryone: false});
+
+//DISCORD.JS V11
+//npm install discord.js@11
+
+
+let token = " "
+let channelidBUMP = "  "
+
+client.login(token).catch((err) => {console.log('Token du bot non fonctionnel')})
+
+
+
+client.on("ready", async => {
+console.log('Bot prêt: '+client.user.username)
+  setInterval(() => {
+        var channel = client.channels.get(channelidBUMP)
+        if(!channel) return ("bump channel invalide")
+  channel.send("!d bump").catch(console.error); // add error handling here
+
+      console.log("bump envoyé ")
+      }, 3600000);
+
+})
